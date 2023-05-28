784dcaad39d7dd7c3ae42de4a567db433b1592d9
emilmarcel <55388306+marcelcph@users.noreply.github.com>
2023-05-28 03:05:00 +0200

    3d canvas

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,43 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from 'three';
 
 const Canvas = () => {
-  const mountRef = useRef(null);
+  const canvasScenen = useRef(null);
+  const [valgteFarve, sætvalgteFarve] = useState(0x00ff00);
 
   useEffect(() => {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 300);
+    const renderer = new THREE.WebGLRenderer({ canvas: canvasScenen.current });
+    renderer.setSize(window.innerWidth * 0.75, window.innerHeight * 0.75);
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    mountRef.current.appendChild(renderer.domElement);
+    const controls = new OrbitControls(camera, renderer.domElement);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00fa });
+    const material = new THREE.MeshBasicMaterial({ color: valgteFarve });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
     camera.position.z = 5;
 
-    const animate = () => {
+    function animate() {
       requestAnimationFrame(animate);
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
       renderer.render(scene, camera);
-    };
-
+    }
     animate();
 
-    // Clean up the scene and renderer on unmount
     return () => {
-      renderer.dispose();
-      scene.remove(cube);
     };
-  }, []);
+  }, [valgteFarve]);
 
-  return <div ref={mountRef} />;
+  const handleColorChange = (event) => {
+    const farveVærdi = parseInt(event.target.value.replace('#', '0x'));
+    sætvalgteFarve(farveVærdi);
+  };
+
+  return (
+    <>
+      <canvas ref={canvasScenen} /> {/* Ref til canvas-elementet */}
+      <input type="color" value={`#${valgteFarve.toString(16)}`} onChange={handleColorChange} style={{ marginTop: '10px' }} /> {/* Color picker input */}
+    </>
+  );
 };
 
 export default Canvas;
